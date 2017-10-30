583fb71a743b08e3b25119c4e18608501913ad15
Baboo7 <baptiste.studer@laposte.net>
2017-10-30 16:37:49 -0700

    set agent client key from environment variable

--- configs/dialogFlow.js
+++ configs/dialogFlow.js
@@ -1,6 +1,6 @@
 const dialogFlow = {
   url: 'https://api.api.ai/v1/query?v=20170712',
-  clientKey: '3ad65bf690c04e8f9ae5625d423db0fd'
+  clientKey: process.env.AGENT_CLIENT_KEY || 'de5f2e493aea4f65b6050e87eee33833'
 };
 
 module.exports = dialogFlow;
