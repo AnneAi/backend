073c5f1c48f709d02fb22211db83c573a6f3b41a
Baboo7 <baptiste.studer@laposte.net>
2017-11-02 21:49:32 -0700

    test

---
+++ test/crypto.js
@@ -0,0 +1,8 @@
+'use strict';
+let expect = require('chai').expect;
+
+describe('crypto', () => {
+  it('should encrypt and decrypt a message correctly', done => {
+    expect(true).to.equal(false);
+  });
+});
